f903b0c65568b561e02ab88340d276809552b791
Muhammad Wasay Khan <khan.wasay@gmail.com>
2020-06-12 05:03:30 +0400

    aded buildspec.yml

--- src/main/java/io/mwk/EksCdkApp.java
+++ src/main/java/io/mwk/EksCdkApp.java
@@ -2,11 +2,26 @@ package io.mwk;
 
 import software.amazon.awscdk.core.App;
 
+import java.util.Map;
+
 public class EksCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new EksCdkStack(app, "EksCdkStack");
+        String defaultImageTag = "latest";
+        String envImageTag = "";
+        String imageTagEnvProperty = "TWITCH_MS_IMAGE_TAG";
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            if(envName.equals(imageTagEnvProperty)){
+                envImageTag = env.get(envName);
+            }
+        }
+        System.out.format("%s=%s%n", imageTagEnvProperty, env.get(envImageTag));
+
+        String imageTag = (envImageTag.isEmpty()) ? defaultImageTag : envImageTag;
+
+        new EksCdkStack(app, "EksCdkStack", envImageTag);
 
         app.synth();
     }
--- src/main/java/io/mwk/EksCdkStack.java
+++ src/main/java/io/mwk/EksCdkStack.java
@@ -12,15 +12,22 @@ import software.amazon.awscdk.services.iam.AccountRootPrincipal;
 import software.amazon.awscdk.services.iam.ManagedPolicy;
 import software.amazon.awscdk.services.iam.Role;
 
+import java.util.Map;
+
 import static java.util.Arrays.asList;
 
 public class EksCdkStack extends Stack {
-    public EksCdkStack(final Construct scope, final String id) {
-        this(scope, id, null);
+    public EksCdkStack(final Construct scope, final String id, String imageTag) {
+        this(scope, id, null, imageTag);
     }
 
-    public EksCdkStack(final Construct scope, final String id, final StackProps props) {
+    public EksCdkStack(final Construct scope, final String id, final StackProps props, String imageTag) {
         super(scope, id, props);
+
+        var deployMicroservice = (imageTag.isBlank()) ? true : false;
+        var twitchMicroserviceImage = "481137230390.dkr.ecr.us-east-1.amazonaws.com/java-twitch-integration:"+imageTag;
+        var twitchAppClientId = "";
+        var twitchAppClientSecret = "";
 
         var clusterAdmin = Role.Builder.create(this, "AdminRole")
                 .assumedBy(new AccountRootPrincipal())
@@ -40,9 +47,43 @@ public class EksCdkStack extends Stack {
                 .cluster(cluster)
                 .instanceType(new InstanceType("t2.micro"))
                 .amiType(NodegroupAmiType.AL2_X86_64)
-                .minSize(1)
-                .maxSize(1)
+                .minSize(2)
+                .maxSize(2)
                 .diskSize(8)
                 .build();
+
+        Map<String, String> appLabel = Map.of("app", "twitch-ms");
+
+        Object deployment = Map.of(
+                "apiVersion", "apps/v1",
+                "kind", "Deployment",
+                "metadata", Map.of("name", "twitch-ms"),
+                "spec", Map.of(
+                        "replicas", 1,
+                        "selector", Map.of("matchLabels", appLabel),
+                        "template", Map.of(
+                                "metadata", Map.of("labels", appLabel),
+                                "spec", Map.of(
+                                        "containers", asList(Map.of(
+                                                "name", "twitch-ms",
+                                                "image", twitchMicroserviceImage,
+                                                "env",asList(Map.of("name","TWITCH_CLIENT_ID",
+                                                        "value", twitchAppClientId),
+                                                        Map.of("name","TWITCH_CLIENT_SECRET",
+                                                                "value", twitchAppClientSecret)),
+                                                "ports", asList(Map.of("containerPort", 8080))))))));
+
+        Object service = Map.of(
+                "apiVersion", "v1",
+                "kind", "Service",
+                "metadata", Map.of("name", "twitch-ms"),
+                "spec", Map.of(
+                        "type", "LoadBalancer",
+                        "ports", asList(Map.of("port", 80, "targetPort", 8080)),
+                        "selector", appLabel));
+
+        if(deployMicroservice){
+            cluster.addResource("twitch-microservice", deployment, service);
+        }
     }
 }
